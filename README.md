a333c4d8769f75bd34afdf667b0b8fc1a7b9aa3f
zulfikar4568 <isnaen70@gmail.com>
2024-05-20 22:43:29 +0700

    feat: opcua server simple

--- README.md
+++ README.md
@@ -1,89 +1,20 @@
-# Typescript Starter
-
-## Create Package
-```
-yarn init -y
-```
-
-## Install dependencies
-```
-typescript ts-node nodemon @types/node
-```
-
-## Add ts config
-```
-npx tsc --init
-```
-
-## Create Source folder src/index.ts
-```ts
-function greet(name: string): void {
-  console.log("Hello", name);
-}
-
-const readerName = "all";
-
-greet(readerName);
-```
-
-## Add nodemon config
-```json
-{
-  "restartable": "rs",
-  "ignore": [".git", "node_modules/", "dist/", "coverage/"],
-  "watch": ["src/"],
-  "execMap": {
-    "ts": "node -r ts-node/register"
-  },
-  "env": {
-    "NODE_ENV": "development"
-  },
-  "ext": "js,json,ts"
-}
-```
-
-- **restartable** — a command we can use to restart the program manually.
-- **ignore** — list of files we don’t want to trigger a program restart when changing.
-- **watch** — list of paths we do want to trigger a program restart when changing.
-- **execMap** — a mapping between a file type/extension to a runtime.
-- **env** — environment variables to include
-- **ext** — the file extensions Nodemon monitores
-
-## Add `script` in `package.json`
-```json
-{
-  "scripts": {
-    "dev": "nodemon --config nodemon.json src/index.ts",
-    "dev:debug": "nodemon --config nodemon.json --inspect-brk src/index.ts"
-  }
-}
-```
-
-## Debugging with vscode
-Create `.vscode/launch.json`
-```json
-{
-  "version": "0.2.0",
-  "configurations": [
-    {
-      "type": "node",
-      "request": "attach",
-      "name": "Attach",
-      "restart": true,
-      "processId": "${command:PickProcess}"
-    }
-  ]
-}
-```
-
-## Running the ts
-```
+## Sample of OPCUA Server
+Run the OPC UA Server
+```bash
 yarn dev
 ```
 
-## Running with debugger
-```
-yarn dev:debug
+## Install OPCUA Client for connecting
+
+[OPCUA Client for Free](https://github.com/FreeOpcUa/opcua-client-gui).
+
+The URL of the OPCUA server that we created it would be `opc.tcp://localhost:26543`. I used mac, so we can install using below commands
+```bash
+brew install pyqt@5
+pip3 install opcua-client pyqtgraph cryptography numpy
 ```
 
-[Source: Medium](https://levelup.gitconnected.com/typescript-nodemon-the-ultimate-setup-7200aa60cc8b)
+Open the OPCUA client
+```bash
+opcua-client
+```
---
+++ src/index.ts
@@ -0,0 +1,109 @@
+import { AddressSpace, DataType, MessageSecurityMode, OPCUAServer, SecurityPolicy, Variant, VariantArrayType } from "node-opcua";
+import os from 'os';
+
+function constructAddressSpace(addressSpace: AddressSpace) {
+  const namespace = addressSpace.getOwnNamespace();
+
+  const myDevice = namespace.addFolder('ObjectsFolder', {
+    browseName: 'MyDevice',
+  });
+
+  const variable1 = 10.0
+
+  // Variable 1
+  namespace.addVariable({
+    componentOf: myDevice,
+    nodeId: 's=Temperature',
+    browseName: 'Temperature',
+    dataType: 'Double',
+    minimumSamplingInterval: 500,
+    accessLevel: 'CurrentRead',
+    value: {
+      get: () => {
+        const time = Date.now() / 10000.0;
+        const value = variable1 + 10.0 * Math.sin(time);
+        return new Variant({ dataType: DataType.Double, value });
+      }
+    }
+  })
+
+  // Variable 2
+  const learnVariable = namespace.addVariable({
+    componentOf: myDevice,
+    browseName: 'MyVariable2',
+    dataType: 'String'
+  });
+  learnVariable.setValueFromSource({
+    dataType: DataType.String,
+    value: 'Learn Node-OPCUA ! Read https://leanpub.com/node-opcuabyexample'
+  });
+
+  // Variable 3
+  const arrayOfData = namespace.addVariable({
+    componentOf: myDevice,
+    browseName: 'MyVariable3',
+    dataType: 'Double',
+    arrayDimensions: [3],
+    accessLevel: 'CurrentRead | CurrentWrite',
+    userAccessLevel: 'CurrentRead | CurrentWrite',
+    valueRank: 1
+
+  });
+  arrayOfData.setValueFromSource({
+    dataType: DataType.Double,
+    arrayType: VariantArrayType.Array,
+    value: [1.0, 2.0, 3.0]
+  });
+
+  // Variable 4
+  namespace.addVariable({
+    componentOf: myDevice,
+    nodeId: 'b=1020ffab',
+    browseName: 'Percentage Memory Used',
+    dataType: 'Double',
+    minimumSamplingInterval: 1000,
+    value: {
+      get: () => {
+        // const value = process.memoryUsage().heapUsed / 1000000;
+        const percentageMemUsed = 1.0 - (os.freemem() / os.totalmem());
+        const value = percentageMemUsed * 100;
+        return new Variant({ dataType: DataType.Double, value });
+      }
+    }
+  })
+}
+
+(async function() {
+  try {
+    const server = new OPCUAServer({
+      port: 26543,
+      buildInfo: {
+        productName: 'Sample of OPCUA Server',
+        buildNumber: '123',
+        buildDate: new Date(2024, 5, 20)
+      },
+      securityPolicies: [SecurityPolicy.None, SecurityPolicy.Basic256Sha256]
+    })
+    
+    await server.initialize();
+
+    if (server.engine.addressSpace !== null) constructAddressSpace(server.engine.addressSpace)
+
+    // we can now start the server
+    await server.start();
+  
+    console.log('Server is now listening ... ( press CTRL+C to stop) ');
+    server.endpoints[0].endpointDescriptions().forEach((endpoint) => {
+      console.log(endpoint.endpointUrl, MessageSecurityMode[endpoint.securityMode], endpoint.securityPolicyUri?.toString().padEnd(60));
+      console.log("    ", endpoint.userIdentityTokens?.map((x) => x.policyId?.toString()).join(' '));
+    });
+    await new Promise((resolve) => process.once("SIGINT",resolve));
+  
+    await server.shutdown();
+    console.log('server shutdown completed !');
+  } catch (err: any) {
+    console.log(err.message);
+    process.exit(-1);
+  }
+})();
+
